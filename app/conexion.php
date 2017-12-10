2783b43d5283c925a94ed1370323ac1171f2572d
Juan de Dios Canche Cen <juan1910@live.com.mx>
2017-12-10 16:52:02 -0600

    claves del servidor

--- app/conexion.php
+++ app/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion = new mysqli("localhost","root","","lavatronic_itm");
+    $conexion = new mysqli("mysql.hostinger.mx","u361262437_juan","Watusi04","u361262437_lava");
     
     if( $conexion->connect_errno ){
         echo "conexion fallida :c ".$conexion->connect_errno;
