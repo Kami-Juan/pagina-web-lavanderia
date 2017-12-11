83c549d893ee9698a74e1084771401b6906d9e54
Juan de Dios Canche Cen <juan1910@live.com.mx>
2017-12-10 22:24:15 -0600

    galeria added

--- views/layouts/header-login.php
+++ views/layouts/header-login.php
@@ -4,7 +4,7 @@
 <div id="time">
 <div class="menu_opciones">
     <a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/pagina-web-lavanderia/views/index.php">Inicio</a>
-    <a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/pagina-web-lavanderia/views/comentarios.php">Comentarios</a>
+    <a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/pagina-web-lavanderia/views/comentarios.php">Galería</a>
     <a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/pagina-web-lavanderia/views/mision.php">¿Quiénes somos?</a>
     <a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/pagina-web-lavanderia/views/ubicacion.php">Ubicación</a>
     <a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/pagina-web-lavanderia/views/comentario.php">Enviar Comentario</a>
